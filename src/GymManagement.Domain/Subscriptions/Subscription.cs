06a28f0ffc1e5616e889347b87cf299caef30baa
Gabriel Oliveira Silva <gabriel.engsist@gmail.com>
2025-01-07 18:25:31 -0300

    chore: add migrations and configuring Subscription model

--- src/GymManagement.Domain/Subscriptions/Subscription.cs
+++ src/GymManagement.Domain/Subscriptions/Subscription.cs
@@ -3,13 +3,18 @@ namespace GymManagement.Domain.Subscriptions;
 public class Subscription
 {
     private readonly Guid _adminId;
-    public Guid Id { get; }
-    public SubscriptionType SubscriptionType { get; } = null!;
+    public Guid Id { get; private set; }
+    public SubscriptionType SubscriptionType { get; private set; } = null!;
 
     public Subscription(SubscriptionType subscriptionType, Guid adminId, Guid? id = null)
     {
         _adminId = adminId;
         Id = id ?? Guid.NewGuid();
         SubscriptionType = subscriptionType;
+    }
+
+    private Subscription()
+    {
+
     }
 }
--- src/GymManagement.Infrastructure/Persistence/SubscriptionsRepository.cs
+++ src/GymManagement.Infrastructure/Persistence/SubscriptionsRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using GymManagement.Application.Common.Interfaces;
 using GymManagement.Domain.Subscriptions;
 using GymManagement.Infrastructure.Common.Persistence;
